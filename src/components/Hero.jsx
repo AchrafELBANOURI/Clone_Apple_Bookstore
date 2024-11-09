ed1bff3b1cddf17bc5f4577867dabc1b2f78e287
Achraf <achrafbnr2406@gmail.com>
2024-11-09 03:25:35 +0100

    Modify The Text in The Main Section of Hero & Use react-icons instead of svg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { FaArrowRight, FaBookOpen } from "react-icons/fa"
+
+
+const Hero_title = (<>Read, listen, discover. <br/> All in one app.</>)
+const Hero_description = "Apple Books is the single destination to find, buy, and dive into audiobooks and ebooks. Browse curated collections and get personalized recommendations. Share your books with up to five family members.* All with no subscription or monthly commitment."
 
 const Hero = () => {
   return (
     <section className="text-gray-900 dark:text-white dark:bg-gray-900">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <FaBookOpen className="mx-auto text-8xl bg-orange-400 py-3 px-4 mb-5 rounded-3xl text-white dark:bg-transparent dark:border-orange-400 dark:border-2"/>
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">
-          We invest in the world’s potential
+          {Hero_title}
         </h1>
         <p className="mb-8 text-lg font-normal lg:text-xl sm:px-16 lg:px-48">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
+          {Hero_description}
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
           <a
@@ -17,21 +22,7 @@ const Hero = () => {
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Get started
-            <svg
-              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
+            <FaArrowRight className="ml-3"/>
           </a>
           <a
             href="#"
