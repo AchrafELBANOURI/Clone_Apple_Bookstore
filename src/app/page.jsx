bbf7c01306c27f3a491cf90fb0e9f7e09b7ebd73
Achraf <achrafbnr2406@gmail.com>
2024-11-09 04:25:45 +0100

    Add Parallax Section With react-scroll-parallax

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
 import Hero from "@/components/Hero";
+import MobileGallery from "@/components/MobileGallery";
 
 export default function Home() {
   return (
-    <Hero/>
+    <>
+      <Hero />
+      <MobileGallery/>
+    </>
   );
 }
