a6c2c434b265b3d7bbfaecb4737b6e38a0e43e16
Achraf <achrafbnr2406@gmail.com>
2024-11-09 18:37:56 +0100

    Develop The Slider Section By Pause it When Hovering

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,6 +4,27 @@ import Slider from './Slider'
 const Filter_title = (<>A library you’ll want <br/>to get lost in.</>)
 const Filter_description = "Browse our global catalog featuring millions of bestsellers, new releases, and classics — with new audiobooks and ebooks added every week."
 
+const Bestselling = [
+  { src : '/assets/images/book_01.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+  { src : '/assets/images/book_02.jpg' , title : 'Conviction' , author : 'Denise Mina' },
+  { src : '/assets/images/book_03.jpg' , title : 'Hillbilly Elegy' , author : 'Sarah J.Maas' },
+  { src : '/assets/images/book_04.jpg' , title : 'Harry Poter' , author : 'Liz Moore' },
+  { src : '/assets/images/book_05.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
+  { src : '/assets/images/book_06.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
+  { src : '/assets/images/book_07.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
+]
+
+const Trending = [
+  { src : '/assets/images/book_08.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+  { src : '/assets/images/book_09.jpg' , title : 'Conviction' , author : 'Denise Mina' },
+  { src : '/assets/images/book_10.jpg' , title : 'Hillbilly Elegy' , author : 'Sarah J.Maas' },
+  { src : '/assets/images/book_11.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+  { src : '/assets/images/book_12.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+  { src : '/assets/images/book_13.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+  { src : '/assets/images/book_14.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
+]
+
+
 const Filter = () => {
   return (
     <section className="text-gray-900 dark:text-white dark:bg-gray-900">
@@ -15,13 +36,14 @@ const Filter = () => {
           {Filter_description}
         </p>
         <div className='flex justify-center'>
-          <ul className='flex flex-col sm:flex-row rounded-xl sm:rounded-full justify-center align-middle gap-3 text-sm bg-gray-200 p-2'>
+          <ul className='flex flex-col sm:flex-row rounded-xl sm:rounded-full justify-center align-middle gap-3 text-sm bg-gray-200 p-2 text-gray-900'>
             <li className='px-4 py-2 rounded-full cursor-pointer select-none hover:bg-gray-900 hover:text-white'>Bestselling Audiobooks</li>
             <li className='px-4 py-2 rounded-full cursor-pointer select-none bg-gray-900 text-white'>New & Trending Ebooks</li>
             <li className='px-4 py-2 rounded-full cursor-pointer select-none hover:bg-gray-900 hover:text-white'>Fan-Favorite Audiobooks</li>
           </ul>
         </div>
-        <Slider/>
+        <Slider images={ Bestselling } duration={ 20 } />
+        <Slider images={ Trending } duration={ 25 } />
       </div>
     </section>
   )
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,26 +2,16 @@ import Image from 'next/image'
 import React from 'react'
 
 
-const Bestselling = [
-    { src : '/assets/images/book_01.jpg' , title : 'Nuclear War' , author : 'Rebecca Yarros' },
-    { src : '/assets/images/book_02.jpg' , title : 'Conviction' , author : 'Denise Mina' },
-    { src : '/assets/images/book_03.jpg' , title : 'Hillbilly Elegy' , author : 'Sarah J.Maas' },
-    { src : '/assets/images/book_04.jpg' , title : 'Harry Poter' , author : 'Liz Moore' },
-    { src : '/assets/images/book_05.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
-    { src : '/assets/images/book_06.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
-    { src : '/assets/images/book_07.jpg' , title : 'Irom Flame' , author : 'Tessa Bailey' },
-]
-
-const Slider = () => {
+const Slider = ({ images , duration }) => {
   return (
     <div className='slider h-72 mt-10 overflow-hidden flex'>
-        <div className='flex gap-5 cursor-pointer animate-swipe'>
+        <div className='flex gap-5 cursor-pointer animate-swipe hover:[animation-play-state:paused]' style={ {animationDuration : `${duration}s` } }>
             {
-                [...Bestselling , ...Bestselling , ...Bestselling ].map(
+                [...images , ...images , ...images ].map(
                     ({src , title , author}) => (
                         <div className='flex flex-col items-start flex-shrink-0'>
                             <Image src={src} alt={title} width={195} height={195}
-                                className='w-52 h-52 object-cover rounded-md '
+                                className='w-52 h-52 object-cover rounded-md'
                             />
                             <span className='pt-2'>{title}</span>
                             <span className='text-gray-400'>{author}</span>
