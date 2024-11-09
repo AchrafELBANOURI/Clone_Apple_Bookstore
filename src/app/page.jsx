8a489ec2b84b967defbca7354fc83fb66c2c9fdf
Achraf <achrafbnr2406@gmail.com>
2024-11-09 19:08:18 +0100

    Add Faq Section With Accordion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Filter from "@/components/Filter";
 import Hero from "@/components/Hero";
 import MobileGallery from "@/components/MobileGallery";
+import Faq from "@/components/Faq"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <MobileGallery/>
       <Filter/>
+      <Faq/>
     </>
   );
 }
